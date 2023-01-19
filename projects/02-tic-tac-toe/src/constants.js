daa2acd6523f669c65c86981c0ff8e7a5fe18263
Sergio Linero <SLineroDev@gmail.com>
2023-01-19 18:02:05 +0100

    Eslint fix to rest of files

--- projects/02-tic-tac-toe/src/constants.js
+++ projects/02-tic-tac-toe/src/constants.js
@@ -1,6 +1,6 @@
 export const TURNS = { // turnos
-  X: '❌', 
-  O: '⚪' 
+  X: '❌',
+  O: '⚪'
 }
 
 export const WINNER_COMBOS = [
--- projects/02-tic-tac-toe/vite.config.js
+++ projects/02-tic-tac-toe/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
 })
