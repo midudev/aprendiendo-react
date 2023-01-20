d72de88558058c21fd9d4dcce9a8195e7ddae788
Gustavo Boubeta <gboubeta@gmail.com>
2023-01-20 10:38:23 +0100

    Mouse follower initial position on enabled

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -27,23 +27,30 @@ const FollowMouse = () => {
     }
   }, [enabled])
 
+  const handleClick = (event) => {
+    const { clientX, clientY } = event
+    setPosition({ x: clientX, y: clientY })
+    setEnabled(!enabled)
+  }
+
   return (
     <>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
-      />
-      <button onClick={() => setEnabled(!enabled)}>
+      {enabled &&
+        <div style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+        />}
+      <button onClick={handleClick}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
     </>
