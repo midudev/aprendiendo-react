4dc9a64971b299d231ac6a3cc5e211c3e7b15fb0
Miguel Ángel Durán <miduga@gmail.com>
2023-04-26 22:08:41 +0200

    Add apiKey

--- projects/12-comments-react-query/src/service/comments.ts
+++ projects/12-comments-react-query/src/service/comments.ts
@@ -7,12 +7,15 @@ export interface CommentWithId extends Comment {
   id: string
 }
 
+// ApiKey could be public as service is 100% free
+const apiKey = "\$2b\$10\$jOpMXFaiNgsyhru7Nt.GouBUmHStWY9IRZR7vCocenxkK.vv7tDsu"
+
 export const getComments = async () => {
   const response = await fetch('https://api.jsonbin.io/v3/b/643fbe2bc0e7653a05a77535', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
-      'X-Access-Key': import.meta.env.VITE_PUBLIC_API_KEY
+      'X-Access-Key': apiKey
     }
   })
 
