063307be5b3a8dd727ede4cc6ab5595ac1cc6639
PG fom78 <fom78a@gmail.com>
2023-04-30 02:41:47 -0300

    fix bugs when refresh with modal active

--- projects/02-tic-tac-toe/src/logic/storage/index.js
+++ projects/02-tic-tac-toe/src/logic/storage/index.js
@@ -1,10 +1,12 @@
-export const saveGameToStorage = ({ board, turn }) => {
+export const saveGameToStorage = ({ board, turn, winner }) => {
   // guardar aqui partida
   window.localStorage.setItem('board', JSON.stringify(board))
   window.localStorage.setItem('turn', turn)
+  window.localStorage.setItem('winner', winner)
 }
 
 export const resetGameStorage = () => {
   window.localStorage.removeItem('board')
   window.localStorage.removeItem('turn')
+  window.localStorage.removeItem('winner')
 }
