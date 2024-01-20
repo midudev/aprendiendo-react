d732c1c7446e20fa921da975a2f82b1d1a784417
Byron2016 <bgva2005@yahoo.com>
2024-01-20 10:45:01 -0500

    Fix broken url for cat image (project 04)

--- projects/04-react-prueba-tecnica/src/hooks/useCatImage.js
+++ projects/04-react-prueba-tecnica/src/hooks/useCatImage.js
@@ -1,23 +1,26 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
-export function useCatImage ({ fact }) {
-  const [imageUrl, setImageUrl] = useState()
+export function useCatImage({ fact }) {
+  const [imageUrl, setImageUrl] = useState();
 
   // para recuperar la imagen cada vez que tenemos una cita nueva
   useEffect(() => {
-    if (!fact) return
+    if (!fact) return;
 
-    const threeFirstWords = fact.split(' ', 3).join(' ')
+    const threeFirstWords = fact.split(" ", 3).join(" ");
 
-    fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        const { url } = response
-        setImageUrl(url)
-      })
-  }, [fact])
+    fetch(
+      `https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true`
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        const { _id } = response;
+        const url = `/cat/${_id}/says/${threeFirstWords}`;
+        setImageUrl(url);
+      });
+  }, [fact]);
 
-  return { imageUrl: `${CAT_PREFIX_IMAGE_URL}${imageUrl}` }
+  return { imageUrl: `${CAT_PREFIX_IMAGE_URL}${imageUrl}` };
 }
